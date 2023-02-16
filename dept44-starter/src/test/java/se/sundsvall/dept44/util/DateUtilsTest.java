d1699cfc1a6e4ce432dbb46f5460778e583ae780
Michael Wallner <Michael.Wallner@Sundsvall.se>
2023-02-16 15:45:52 +0100

    UF-4249: Added workflow for deploying new version to maven-central.

--- dept44-starter/src/test/java/se/sundsvall/dept44/util/DateUtilsTest.java
+++ dept44-starter/src/test/java/se/sundsvall/dept44/util/DateUtilsTest.java
@@ -1,45 +1,63 @@
 package se.sundsvall.dept44.util;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.time.OffsetTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.chrono.ThaiBuddhistDate;
 import java.time.temporal.Temporal;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DateUtilsTest {
 
 	@Test
 	void toOffsetDateTimeWithLocalOffset() {
 
-		// Zulu time zone into time with local offset +1h.
-		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(OffsetDateTime.parse("2021-11-10T09:23:42.500Z"))).hasToString("2021-11-10T10:23:42.500+01:00");
+		// Create Offset
+		final var zoneOffsetWinter = OffsetDateTime.parse("2021-11-10T09:23:42.500Z").atZoneSameInstant(ZoneId.systemDefault()).getOffset();
+		final var zoneOffsetSummer = OffsetDateTime.parse("2021-06-10T09:23:42.500Z").atZoneSameInstant(ZoneId.systemDefault()).getOffset();
 
-		// Zulu time zone into time with local offset +2h, with DST.
-		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(OffsetDateTime.parse("2021-06-10T09:23:42.500Z"))).hasToString("2021-06-10T11:23:42.500+02:00");
+		final var winterOffsetDateTimeWithoutZuluTimeZone = OffsetDateTime.parse("2021-11-10T09:23:42.500Z");
+		final var summerOffsetDateTimeWithoutZuluTimeZone = OffsetDateTime.parse("2021-06-10T09:23:42.500Z");
+		final var winterOffsetDateTimeWithPlusFour = OffsetDateTime.parse("2021-11-10T12:23:42.500+04:00");
+		final var winterOffsetDateTimeWithMinusOne = OffsetDateTime.parse("2021-11-10T12:23:42.500-01:00");
+		final var winterLocalDateTimeNoOffset = LocalDateTime.parse("2021-11-10T12:23:42");
+		final var summerLocalDateTimeNoOffset = LocalDateTime.parse("2021-06-10T12:23:42");
 
-		// Time with offset +4h into time with local offset +1h.
-		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(OffsetDateTime.parse("2021-11-10T12:23:42.500+04:00"))).hasToString("2021-11-10T09:23:42.500+01:00");
+		// Zulu time zone into time with local offset +1h in Sweden.
+		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(winterOffsetDateTimeWithoutZuluTimeZone))
+			.hasToString(winterOffsetDateTimeWithoutZuluTimeZone.atZoneSameInstant(zoneOffsetWinter).toString());
 
-		// Time with offset -1h into time with local offset +1h.
-		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(OffsetDateTime.parse("2021-11-10T12:23:42.500-01:00"))).hasToString("2021-11-10T14:23:42.500+01:00");
+		// Zulu time zone into time with local offset +2h, with DST in Sweden.
+		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(summerOffsetDateTimeWithoutZuluTimeZone))
+			.hasToString(summerOffsetDateTimeWithoutZuluTimeZone.atZoneSameInstant(zoneOffsetSummer).toString());
 
-		// Time with no offset (e.q. localDateTime) into time with local offset +1h.
-		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(LocalDateTime.parse("2021-11-10T12:23:42"))).hasToString("2021-11-10T12:23:42+01:00");
+		// Time with offset +4h into time with local offset +1h in Sweden.
+		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(winterOffsetDateTimeWithPlusFour))
+			.hasToString(winterOffsetDateTimeWithPlusFour.atZoneSameInstant(zoneOffsetWinter).toString());
 
-		// Time with no offset (e.q. localDateTime) into time with local offset +2h.
-		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(LocalDateTime.parse("2021-06-10T12:23:42"))).hasToString("2021-06-10T12:23:42+02:00");
+		// Time with offset -1h into time with local offset +1h in Sweden.
+		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(winterOffsetDateTimeWithMinusOne))
+			.hasToString(winterOffsetDateTimeWithMinusOne.atZoneSameInstant(zoneOffsetWinter).toString());
+
+		// Time with no offset (e.q. localDateTime) into time with local offset +1h in Sweden.
+		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(winterLocalDateTimeNoOffset))
+			.hasToString(winterLocalDateTimeNoOffset.atOffset(zoneOffsetWinter).toString());
+
+		// Time with no offset (e.q. localDateTime) into time with local offset +2h in Sweden.
+		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(summerLocalDateTimeNoOffset))
+			.hasToString(summerLocalDateTimeNoOffset.atOffset(zoneOffsetSummer).toString());
 
 		// Null input renders null output.
 		assertThat(DateUtils.toOffsetDateTimeWithLocalOffset(null)).isNull();

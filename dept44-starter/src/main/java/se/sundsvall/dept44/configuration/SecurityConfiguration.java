a00449edaa7f9b729ebac304ecb6facf2df3f014
Martin Hansson <martinhansson80@gmail.com>
2023-09-01 10:35:54 +0200

    uf-5772: fixed vulnerability issue regarding requestMatchers

--- dept44-starter/src/main/java/se/sundsvall/dept44/configuration/SecurityConfiguration.java
+++ dept44-starter/src/main/java/se/sundsvall/dept44/configuration/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +23,6 @@ public class SecurityConfiguration {
 
     @Bean
     WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().requestMatchers("/**");
+        return web -> web.ignoring().requestMatchers(AntPathRequestMatcher.antMatcher("/**"));
     }
 }

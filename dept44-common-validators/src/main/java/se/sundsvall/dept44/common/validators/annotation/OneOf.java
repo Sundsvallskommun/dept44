0a70ff35bfac745f5538a2175d3204b059d4e4ab
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2023-10-03 10:26:31 +0200

    UF-5979 - OneOf validator
    Adds a custom validator for ensuring that a value exists in a pre-defined list
    of allowed values

---
+++ dept44-common-validators/src/main/java/se/sundsvall/dept44/common/validators/annotation/OneOf.java
@@ -0,0 +1,31 @@
+package se.sundsvall.dept44.common.validators.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import se.sundsvall.dept44.common.validators.annotation.impl.OneOfConstraintValidator;
+/**
+ * The annotated element must exist within a given list of allowed values.
+ */
+@Documented
+@Target({ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = OneOfConstraintValidator.class)
+public @interface OneOf {
+
+	String message() default "must be one of: {allowedValues}";
+
+	Class<?>[] groups() default { };
+
+	Class<? extends Payload>[] payload() default {};
+
+	String[] value();
+
+	boolean nullable() default false;
+}
---
+++ dept44-common-validators/src/main/java/se/sundsvall/dept44/common/validators/annotation/impl/OneOfConstraintValidator.java
@@ -0,0 +1,60 @@
+package se.sundsvall.dept44.common.validators.annotation.impl;
+
+import static java.util.Objects.isNull;
+import static java.util.Optional.ofNullable;
+import static org.springframework.util.ReflectionUtils.findMethod;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import org.hibernate.validator.internal.engine.constraintvalidation.ConstraintValidatorContextImpl;
+
+import se.sundsvall.dept44.common.validators.annotation.OneOf;
+/**
+ * Defines the logic to validate that a string exists in a list of allowed values.
+ */
+public class OneOfConstraintValidator extends AbstractValidator implements ConstraintValidator<OneOf, String> {
+
+	private boolean nullable;
+	private List<String> allowedValues;
+
+	@Override
+	public void initialize(final OneOf annotation) {
+		nullable = annotation.nullable();
+		allowedValues = Arrays.stream(annotation.value()).toList();
+	}
+
+	@Override
+	public boolean isValid(final String value, final ConstraintValidatorContext context) {
+		if (isNull(value) && nullable) {
+			return true;
+		}
+
+		if (!allowedValues.contains(value)) {
+			if (!isNull(context)) {
+				((ConstraintValidatorContextImpl) context).addMessageParameter("allowedValues", allowedValues);
+			}
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	boolean isValid(final String value) {
+		return isValid(value, null);
+	}
+
+	@Override
+	String getMessage() {
+		return ofNullable(findMethod(OneOf.class, MESSAGE_METHOD_NAME))
+			.map(Method::getDefaultValue)
+			.map(Object::toString)
+			.map(message -> message.replace("{allowedValues}", String.valueOf(allowedValues)))
+			.orElseThrow(createException(OneOf.class.getName()));
+	}
+}

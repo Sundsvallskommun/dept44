00e59dc45ce7c5dd95c20dccf014a83f4444b86b
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2024-09-05 09:51:31 +0200

    UF-9830- Enum-backed constraint
    Adds a validation constraint for making sure that an annotated element is
    equal to a type of a given enum class, optionally without regard to
    case-sensitivity.

---
+++ dept44-common-validators/src/main/java/se/sundsvall/dept44/common/validators/annotation/BackedByEnum.java
@@ -0,0 +1,35 @@
+package se.sundsvall.dept44.common.validators.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import se.sundsvall.dept44.common.validators.annotation.impl.BackedByEnumConstraintValidator;
+
+/**
+ * The annotated element must be equal to a type of a given enum class, optionally without regard to
+ * case-sensitivity.
+ */
+@Documented
+@Target({ ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_USE })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = BackedByEnumConstraintValidator.class)
+public @interface BackedByEnum {
+
+	String message() default "must be one of: {allowedValues}{caseSensitivity}";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	Class<? extends Enum<?>> value();
+
+	boolean nullable() default false;
+
+	boolean caseSensitive() default true;
+}
---
+++ dept44-common-validators/src/main/java/se/sundsvall/dept44/common/validators/annotation/impl/BackedByEnumConstraintValidator.java
@@ -0,0 +1,83 @@
+package se.sundsvall.dept44.common.validators.annotation.impl;
+
+import static java.util.Comparator.nullsLast;
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toCollection;
+import static org.springframework.util.ReflectionUtils.findMethod;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Supplier;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
+
+import se.sundsvall.dept44.common.validators.annotation.BackedByEnum;
+import se.sundsvall.dept44.common.validators.annotation.OneOf;
+
+/**
+ * Defines the logic to validate that a string matches a type of a given enum class.
+ */
+public class BackedByEnumConstraintValidator extends AbstractValidator implements ConstraintValidator<BackedByEnum, String> {
+
+	private static final Supplier<Set<String>> CASE_SENSITIVE = HashSet::new;
+	private static final Supplier<Set<String>> CASE_INSENSITIVE = () -> new TreeSet<>(nullsLast(String.CASE_INSENSITIVE_ORDER));
+
+	private boolean nullable;
+	private boolean caseSensitive;
+	private Set<String> allowedValues;
+
+	@Override
+	public void initialize(final BackedByEnum annotation) {
+		nullable = annotation.nullable();
+		caseSensitive = annotation.caseSensitive();
+		allowedValues = Arrays.stream(annotation.value().getEnumConstants())
+			.map(Enum::name)
+			.collect(toCollection(caseSensitive ? CASE_SENSITIVE : CASE_INSENSITIVE));
+	}
+
+	@Override
+	public boolean isValid(final String value, final ConstraintValidatorContext context) {
+		if (isNull(value) && nullable) {
+			return true;
+		}
+
+		if (!allowedValues.contains(value)) {
+			ofNullable(context)
+				.map(ctx -> ctx.unwrap(HibernateConstraintValidatorContext.class))
+				.ifPresent(ctx -> {
+					ctx.addMessageParameter("allowedValues", allowedValues);
+					ctx.addMessageParameter("caseSensitivity", caseSensitive ? "" : " (case-insensitive)");
+				});
+
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	boolean isValid(final String value) {
+		return isValid(value, null);
+	}
+
+	@Override
+	String getMessage() {
+		return ofNullable(findMethod(OneOf.class, MESSAGE_METHOD_NAME))
+			.map(Method::getDefaultValue)
+			.map(Object::toString)
+			.map(message -> message
+				.replace("{allowedValues}", String.valueOf(allowedValues))
+				.replace("{caseSensitivity}", caseSensitive ? "" : " (case-insensitive)"))
+			.orElseThrow(createException(OneOf.class.getName()));
+	}
+}

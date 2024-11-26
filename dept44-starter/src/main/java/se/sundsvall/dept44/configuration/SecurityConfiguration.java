b2974bd479f27f60f22e4c580b021ef94b61c134
cheezi0747 <linus.sjolinder@b3.se>
2024-11-26 10:55:12 +0100

    Fixed conflicting filterChain

--- dept44-starter/src/main/java/se/sundsvall/dept44/configuration/SecurityConfiguration.java
+++ dept44-starter/src/main/java/se/sundsvall/dept44/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package se.sundsvall.dept44.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -12,8 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
 	@Bean
+	@Order(0)
 	SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
 		return http
+			.securityMatcher("/**")
 			.authorizeHttpRequests(authorizeHttpRequestsCustomizer -> authorizeHttpRequestsCustomizer.anyRequest().permitAll())
 			.build();
 	}
